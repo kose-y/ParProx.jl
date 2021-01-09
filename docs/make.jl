8fedd4c3db691ff7654ddade60f6d906675f6bb8
Seyoon Ko <syko0507@snu.ac.kr>
2021-01-10 00:05:54 +0900

    make public

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,17 @@
-using Documenter, ProxCox
+using Documenter, ParProx
 
 makedocs(;
-    modules=[ProxCox],
+    modules=[ParProx],
     format=Documenter.HTML(),
     pages=[
         "Home" => "index.md",
     ],
     repo="https://github.com/kose-y/ProxCox.jl/blob/{commit}{path}#L{line}",
-    sitename="ProxCox.jl",
+    sitename="ParProx.jl",
     authors="Seyoon Ko",
     assets=String[],
 )
 
 deploydocs(;
-    repo="github.com/kose-y/ProxCox.jl",
+    repo="github.com/kose-y/ParProx.jl",
 )
--- src/cv.jl
+++ src/cv.jl
@@ -130,7 +130,11 @@ function cross_validate(u::COXUpdate, X::Matrix, δ::Vector, t::Vector, penaltie
         for (i, p) in enumerate(penalties)
             V.penalty = p
             V.obj_prev = -Inf
-            @time fit!(u, V)
+            if u.verbose
+                @time fit!(u, V)
+            else
+                fit!(u, V)
+            end
             scores[i, j] = cindex(t_test, δ_test, X_test, adapt(Array{T}, V.β))
         end
     end
@@ -175,7 +179,11 @@ function cross_validate(u::COXUpdate, X::AbstractMatrix, X_unpen::AbstractMatrix
             p = GroupNormL2(l, grpidx)
             V.penalty = p
             V.obj_prev = -Inf
-            @time fit!(u, V)
+            if u.verbose
+                @time fit!(u, V)
+            else
+                fit!(u, V)
+            end
             scores[i, j] = cindex(t_test, δ_test, X_test, V.β)
         end
     end
@@ -214,7 +222,11 @@ function cross_validate(u::LogisticUpdate, X::Matrix, y::Vector, penalties::Vect
         for (i, p) in enumerate(penalties)
             V.penalty = p
             V.obj_prev = -Inf
-            @time fit!(u, V)
+            if u.verbose
+                @time fit!(u, V)
+            else
+                fit!(u, V)
+            end
             scores[i, j] = criteria(y_test, X_test, adapt(Array{T}, V.β))
         end
     end
@@ -257,7 +269,11 @@ function cross_validate(u::LogisticUpdate, X::AbstractMatrix, X_unpen::AbstractM
             p = GroupNormL2(l, grpidx)
             V.penalty = p
             V.obj_prev = -Inf
-            @time fit!(u, V)
+            if u.verbose
+                @time fit!(u, V)
+            else
+                fit!(u, V)
+            end
             scores[i, j] = criteria(y_test, X_test, V.β)
         end
     end
